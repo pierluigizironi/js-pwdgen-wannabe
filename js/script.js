90b346458ad70e226e72f2c37e6cb599a76dba03
pierluigizironi <zironipierluigi@gmail.com>
2021-12-20 15:14:14 +0100

    add human code and first console log

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('JS OK')
+
+/* Chiedere all'utente nome, cognome e colore preferito a cui 
+si aggiungerà in automatico 
+ il numero 21 */
+
+/* 
+1 Recupero il nome utente
+2 Recupero il cognome utente 
+3 Recupero il colore preferito
+4 Li unisco
+5 aggiungo il numero 21 alla fine
+*/
