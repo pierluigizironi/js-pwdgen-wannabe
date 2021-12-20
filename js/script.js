12bcd5ab75e2c8ea7950367ceea41b423cb34ba2
pierluigizironi <zironipierluigi@gmail.com>
2021-12-20 15:30:02 +0100

    add question with prompt method

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ si aggiungerà in automatico
 4 Li unisco
 5 aggiungo il numero 21 alla fine
 */
+
+// 1 Chiedo qual è il nome
+
+const userName = prompt('Come ti chiami?')
+console.log (userName)
+
+// 2 Chiedo qual è il cognome
+
+const userSurname = prompt('E il tuo cognome?')
+console.log (userSurname)
+
+// 2 Recupero il colore preferito dell'utente
+
+const userColor = prompt('Dimmi anche il tuo colore preferito!')
+console.log (userColor)
